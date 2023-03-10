8f7cbe0cbeb13736e21b73918760ece4d3af4324
dev-xpeace <xpeacedev@outlook.com>
2023-03-11 02:27:35 +0300

    Mad Lib Game
    A simple mad lib game on C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string color, pluralNoun, celebrity;
+
+Console.Write("Enter a color: ");
+color = Console.ReadLine();
+Console.Write("Enter a plural noun: ");
+pluralNoun = Console.ReadLine();
+Console.Write("Enter a celebrity: ");
+celebrity = Console.ReadLine();
+
+Console.WriteLine("Roses are " + color);
+Console.WriteLine(pluralNoun + " are blue");
+Console.WriteLine("I love " +  celebrity);
+
+Console.ReadLine();
